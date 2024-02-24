a95271242b02ca470fe2cf4370f988ed2c64f818
Fatimakhan0 <124853832+Fatimakhan0@users.noreply.github.com>
2024-02-24 15:19:10 +0530

    Add files via upload

---
+++ Arithemetic.java
@@ -0,0 +1,12 @@
+class Arithemetic
+{
+  public static void main (String[] args)
+  {
+   int a=5,b=15;
+   System.out.println("Addition of "+a+" & "+b+" is "+(a+b));
+   System.out.println("Subtraction of "+a+" & "+b+" is "+(a-b));
+   System.out.println("Multiplication of "+a+" & "+b+" is "+(a*b));
+   System.out.println("Division of "+a+" & "+b+" is "+(a/b));
+   System.out.println("Qoutient of "+a+" & "+b+" is "+(a%b));
+  }
+}
