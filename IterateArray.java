9c635925c7efd2b996c0803b584417ba2e218490
Fatimakhan0 <124853832+Fatimakhan0@users.noreply.github.com>
2024-03-28 11:51:50 +0530

    Add files via upload

---
+++ IterateArray.java
@@ -0,0 +1,18 @@
+class IterateArray
+{
+	public static void main(String[] ar)
+	{
+		int[] arr={1,2,4,5,6,7};
+		//CommonElement co=new CommonElement();
+		/*for(int 1:co.arr3)
+		{
+			System.out.println(1);
+		}*/
+		/*CommonElement.main(null);*/
+		System.out.println("Elements are:");
+		for (int i=0;i<arr.length;i++)
+		{
+			System.out.println(arr[i]);
+		}
+	}
+}
