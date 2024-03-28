84c93fe7968933aa562531651f5cb15b55500930
Fatimakhan0 <124853832+Fatimakhan0@users.noreply.github.com>
2024-03-28 12:00:22 +0530

    Update IterateArray.java

--- IterateArray.java
+++ IterateArray.java
@@ -3,13 +3,15 @@ class IterateArray
 	public static void main(String[] ar)
 	{
 		int[] arr={1,2,4,5,6,7};
-		//CommonElement co=new CommonElement();
-		/*for(int 1:co.arr3)
+		CommonElement co=new CommonElement();
+		System.out.println("Array 3 :");
+		for(int l:co.arr3)
 		{
-			System.out.println(1);
-		}*/
-		/*CommonElement.main(null);*/
-		System.out.println("Elements are:");
+			System.out.println(l);
+		}
+		System.out.println("Common elements :");
+		CommonElement.main(null);
+		System.out.println("Elements to iterate are:");
 		for (int i=0;i<arr.length;i++)
 		{
 			System.out.println(arr[i]);
